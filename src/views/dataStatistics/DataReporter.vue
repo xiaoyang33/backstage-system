41d1ac7b9cb8aee23e31a526e108a0d2ab1d4a49
raolei <1170902454@qq.com>
2020-09-18 14:58:47 +0800

    init

---
+++ src/views/dataStatistics/DataReporter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        我是折线
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>

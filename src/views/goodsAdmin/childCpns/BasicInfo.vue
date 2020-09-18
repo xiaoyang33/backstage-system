7649bf0ddccbe1353b73aa7ef8af5d08caa3d4f5
yangwenlong <81355146@qq.com>
2020-09-18 15:20:30 +0800

    init

---
+++ src/views/goodsAdmin/childCpns/BasicInfo.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="form-item">
+      <div>
+        <i>* </i>
+        <span> 商品名称</span>
+      </div>
+      <Input v-model="fromInfo.goodsName" placeholder="请输入商品名称" width="100%" />
+    </div>
+    <div class="form-item">
+      <div>
+        <i>* </i>
+        <span> 商品价格</span>
+      </div>
+      <Input v-model="fromInfo.goodsPrice" placeholder="请输入商品价格" width="100%" />
+    </div>
+    <div class="form-item">
+      <div>
+        <i>* </i>
+        <span> 商品重量</span>
+      </div>
+      <Input v-model="fromInfo.goodsWeight" placeholder="请输入商品重量" width="100%" />
+    </div>
+    <div class="form-item">
+      <div>
+        <i>* </i>
+        <span> 商品数量</span>
+      </div>
+      <Input v-model="fromInfo.goodsNum" type="number" min="0" placeholder="请输入商品数量" width="100%" />
+    </div>
+    <div class="selCate">
+      <div>
+        <i>* </i><span>请选择商品分类：</span>
+      </div>
+      <Cascader
+        @on-change="fin"
+        style="width:250px;z-index:999"
+        :data="cascader"
+        change-on-select
+        trigger="hover"
+      ></Cascader>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getCate, getCateAll} from '../../../network/goodsAdmin'
+export default {
+    data(){
+      return {
+        fromInfo:{
+          goodsName:'',
+          goodsPrice:0,
+          goodsWeight:0,
+          goodsNum:0,
+          selectId:''
+        },
+       cascader:[],
+      }
+    },
+    methods:{
+      format(val) {
+      if (val === undefined) return [];
+      let arr = [];
+      val.forEach((item) => {
+        let obj = {
+          value: item.cat_name,
+          label: item.cat_name,
+          cat_pid: item.cat_pid,
+          cat_name: item.cat_name,
+          cat_level: item.cat_level,
+          cat_id: item.cat_id,
+          children: this.format(item.children),
+        };
+        arr.push(obj);
+      });
+      return arr;
+    },
+    fin(e,a){
+        console.log(a);
+        let str = ''
+        a.forEach(item=>{
+          str+=item.cat_id+','
+        })
+        str=str.substring(0,str.length-1)
+        console.log(str);
+        this.fromInfo.selectId = str
+    }
+    },
+    created(){
+      getCateAll().then(res=>{
+        console.log(res)
+        this.cascader = this.format(res.data.data)
+      })
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.form-item{
+  div{
+    margin-bottom: 15px;
+    width: 100%;
+    i{
+      color: red;
+    }
+  }
+}
+.selCate {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  span {
+    font-size: 16px;
+  }
+  i{
+    color: red;
+  }
+  &>div{
+    margin-bottom: 10px;
+  }
+}
+</style>
